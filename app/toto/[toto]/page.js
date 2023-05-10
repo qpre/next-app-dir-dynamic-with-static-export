aa8217a72de7d9d319aadb0b84ebd17375f997a3
Steven <steven@ceriously.com>
2023-05-10 14:49:23 -0400

    Add generateStaticParams()

--- app/toto/[toto]/page.js
+++ app/toto/[toto]/page.js
@@ -1,4 +1,7 @@
-'use client'
+export async function generateStaticParams() {
+  return [{toto: 'foo'}, {toto: 'bar'}];
+}
+
 
 export default function TotoPage({ params }) {
   return <div>{params.toto}</div>;
